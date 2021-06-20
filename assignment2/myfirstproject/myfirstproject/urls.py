c3397e94b00adbb45c1b915e08b98740fae1b798
yunyoungyee <eric3725@naver.com>
2021-06-20 23:21:15 +0900

    instagram

--- assignment2/myfirstproject/myfirstproject/urls.py
+++ assignment2/myfirstproject/myfirstproject/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from myfirstapp.views import *
+from django.urls import path, include
+from myfirstapp.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home,name="home"),
-    path('<str:id>',detail,name="detail"),
+    path('blog/', include('blog.urls')),
 ]
